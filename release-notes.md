29b6425904f19e4e1fc08d7e609a51fa6ea56890
Amit Wertheimer <12250123+amitwer@users.noreply.github.com>
2024-07-02 23:44:28 +0300

    release ready!

--- release-notes.md
+++ release-notes.md
@@ -1,3 +1,7 @@
+# version 1.0.0
+* Added check for release notes
+* Added two utility functions: get_test_session_data & get_test_item_data
+* Added a field xdist_worker_id in TestSessionItem - None if there is no such env variable
 # version 0.0.3
 Fix bug with xdist having a null testrunid, add session start and end to TestSessionData
 # version 0.0.2
--- src/pytest_stats/test_session_data.py
+++ src/pytest_stats/test_session_data.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
+
 class TestSessionData:
     session_id: str
+    xdist_worker_id: Optional[str]
     status: str
     failed_tests: int
     collected_tests: int
-    start_time:float
-    end_time:float
+    start_time: float
+    end_time: float
 
     def __str__(self: 'TestSessionData') -> str:
         return f'<{self.__class__.__name__}: {str(vars(self))}>'
